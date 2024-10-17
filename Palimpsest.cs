95bab2126ae2ff303cb7af6d535f6a58a6a21e8c
Eldon <Birdheldon@gmail.com>
2024-10-17 00:22:27 -0600

    starting up the project

---
+++ Palimpsest.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Encrypt {
+
+    public static String EncryptionWithKey(String key){
+
+
+
+        return "";
+    }
+
+    public static String DecryptWithKey(String key){
+
+
+        return "";
+    }
+}
